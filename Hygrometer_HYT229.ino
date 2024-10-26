c379fb4960ccac5b0d8c5fc9ef8c8f7c7175c31b
Lecostarius <thomas@kemp24.de>
2024-10-26 09:54:45 +0200

    added Barometer

--- Hygrometer_HYT229.ino
+++ Hygrometer_HYT229.ino
@@ -1,8 +1,9 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "Zanshin_BME680.h"
 
-#define OLED_ADDR   0x3D
+#define OLED_ADDR   0x3C
 #define HYT221_ADDR 0x28
 
 #define SCALE_MAX 16384.0
@@ -12,6 +13,8 @@
 
 #define HYT_DEBUG 1
 // https://forum.arduino.cc/t/hyt-221-from-hygrosens-instruments/54560/5
+
+BME680_Class BME680;  ///< Create an instance of the BME680 class
 
 class HYT221 {
     private:
@@ -129,11 +132,32 @@ double HYT221::getTemperature( void ) {
 
 // here we go:
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 HYT221 hyt(HYT221_ADDR);
 
 uint8_t have_hyt = 0;
 uint8_t have_oled = 0;
+
+// BME sensor stuff
+int32_t temp, humidity, pressure, gas;
+
+///< Forward function declaration with default value for sea level
+float altitude(const int32_t press, const float seaLevel = 1013.25);
+float altitude(const int32_t press, const float seaLevel) {
+  /*!
+  @brief     This converts a pressure measurement into a height in meters
+  @details   The corrected sea-level pressure can be passed into the function if it is known,
+             otherwise the standard atmospheric pressure of 1013.25hPa is used (see
+             https://en.wikipedia.org/wiki/Atmospheric_pressure) for details.
+  @param[in] press    Pressure reading from BME680
+  @param[in] seaLevel Sea-Level pressure in millibars
+  @return    floating point altitude in meters.
+  */
+  static float Altitude;
+  Altitude =
+      44330.0 * (1.0 - pow(((float)press / 100.0) / seaLevel, 0.1903));  // Convert into meters
+  return (Altitude);
+}  // of method altitude()
 
 void setup() {
   int ack;
@@ -162,14 +186,34 @@ void setup() {
 
   hyt.begin();
   
-  display.setCursor(0,25);
+  display.setCursor(0,16);
   display.print("HYT: "); display.println(have_hyt);
   display.display();
+
+  // BME680
+  Serial.print(F("Starting I2CDemo example program for BME680\n"));
+  Serial.print(F("- Initializing BME680 sensor\n"));
+  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
+    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
+    delay(5000);
+  }  // of loop until device is located
+  Serial.print(F("- Setting 16x oversampling for all sensors\n"));
+  BME680.setOversampling(TemperatureSensor, Oversample16);  // Use enumerated type values
+  BME680.setOversampling(HumiditySensor, Oversample16);     // Use enumerated type values
+  BME680.setOversampling(PressureSensor, Oversample16);     // Use enumerated type values
+  
+  Serial.print(F("- Setting IIR filter to a value to OFF\n"));
+  //BME680.setIIRFilter(IIR4);  // Use enumerated type values
+  BME680.setIIRFilter(IIROff); // 
+  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));  // "�C" symbols
+  BME680.setGas(320, 150);  // 320�c for 150 milliseconds
 }
 
 
 void loop(){
   Serial.println("loop!");
+  BME680.getSensorData(temp, humidity, pressure, gas); 
+  Serial.print("BME680:"); Serial.print(temp); Serial.print(", pressure "); Serial.println(pressure);
    hyt.read();
    Serial.println("finished read");
    double h = hyt.getHumidity();
@@ -185,9 +229,14 @@ void loop(){
    Serial.println(hraw);
   display.clearDisplay();
   display.setCursor(0,0);
-  display.print(t); display.print(" C");
-  display.setCursor(0,25);
-  display.print(h); display.print(" %rF");
+  display.print(t); display.print(" C / ");
+  display.print(temp / 100.0); display.print(" C");
+  display.setCursor(0,12);
+  display.print(h); display.print(" %rF / ");
+  display.print(humidity); display.print(" %rF");
+  display.setCursor(0,24);
+  display.print(pressure/100.0); display.print(" mbar. Gas: ");
+  display.print(gas);
   display.display();
    delay(3000);
 
